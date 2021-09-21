c2f8505a75ec3ed68581678edebf974678494a33
Denis <uhanov.denis@gmail.com>
2021-09-21 17:56:27 +0300

    Добавьте файлы проекта.

---
+++ Person/Person/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Person
+{
+    class Person
+    {
+        public string name = "Денис";
+        public int age = 18;
+
+        public void GetInfo()
+        {
+            System.Console.WriteLine($"name: {name}, age: {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            Person Denis = new Person();
+            Denis.GetInfo();
+            System.Console.ReadKey();
+
+
+            Denis.name = "Денис";
+            Denis.age = 18;
+            Denis.GetInfo();
+            System.Console.ReadKey();
+
+        }
+
+
+    }
+
+
+}
